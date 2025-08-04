1dbea7332d827520fc63e2bae27b5375ea11366b
z2w <15000941927@163.com>
2025-08-04 08:20:38 +0800

    完成跳表落盘加载测试成功

--- kv_store_skiptable.h
+++ kv_store_skiptable.h
@@ -8,11 +8,25 @@ extern "C" {
 #define MAX_LEVEL   32
 #define P           0.5 /*节点提升概率*/
 
+/*
+节点内存布局：
++-------------------+
+| key  (固定长度)    |
++-------------------+
+| value(固定长度)    |
++-------------------+
+| forward[0]       | --> 层0的下个节点
+| forward[1]       | --> 层1的下个节点
+| ...              |
+| forward[level]   | --> 最高层的下个节点
++-------------------+
+*/
+
 typedef struct Node 
 {
     char key[256];
     char value[256];
-    struct Node** forward; /*前向指针数组（每层一个指针）*/
+    struct Node** forward; 
 } Node;
 
 typedef struct SkipTable
